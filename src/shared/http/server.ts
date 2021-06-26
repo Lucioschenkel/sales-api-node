28e1ae2a701239b3578fe3420a05f58120c0246b
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2021-06-26 09:26:06 -0300

    implements orders routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import '@shared/typeorm';
 import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
 
